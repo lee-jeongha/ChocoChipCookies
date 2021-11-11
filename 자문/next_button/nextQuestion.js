b547551c65da97fdc853cd4b3008af1df5a715ad
jmirrinae <jmirrinae@gmail.com>
2021-11-11 18:24:51 +0900

    next question with tts and stt

--- 자문/next_button/nextQuestion.js
+++ 자문/next_button/nextQuestion.js
@@ -1,10 +1,10 @@
 //DOM
-const NextButton = document.querySelector(".next-button");
+const endButton = document.querySelector(".end-interview");
 
 //function
 function print_name(list) {
     // 결과를 표시할 element
-    const resultElement = document.getElementById('result');
+    const resultElement = document.getElementById('question');
     const countElement = document.getElementById('count');
 
     // 현재 화면에 표시된 값
@@ -33,11 +33,11 @@ function print_name(list) {
 //Web Speech API의 speechSynthesis를 이용하여 tts 구현하기
 function speak(text, opt_prop) {
     if (typeof SpeechSynthesisUtterance === "undefined" || typeof window.speechSynthesis === "undefined") {
-        alert("이 브라우저는 음성 합성을 지원하지 않습니다.")
+        alert("이 브라우저는 tts를 지원하지 않습니다.")
         return
     }
 
-    window.speechSynthesis.cancel() // 현재 읽고있다면 초기화
+    window.speechSynthesis.cancel() // 현재 문장을 읽고 있다면 초기화
 
     const prop = opt_prop || {}
 
@@ -49,4 +49,54 @@ function speak(text, opt_prop) {
 
     // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
     window.speechSynthesis.speak(speechMsg)
+}
+
+function listen(){
+
+    if (!("webkitSpeechRecognition" in window)) {
+        alert("이 브라우저는 stt를 지원하지 않습니다.")
+        return
+    }
+
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    let recognition = new SpeechRecognition();
+    recognition.interimResults = true; //임시 결과 반환 여부를 제어. true는 실시간으로 인식된 결과 확인, false는 최종 결과만 확인
+    recognition.lang = 'ko-KR'; //언어는 한국어
+    recognition.continuous = true; //기본값은 false. true로 설정할 경우, 각각 인식된 문장을 하나로 합쳐주며, 중간에 쉬어도 stop되지 않음.
+
+    //새로운 문단을 추가하는 함수
+    let makeNewTextContent = function() {
+        p = document.createElement('p'); //'p'라는 이름의 Element를 생성한다.
+        document.querySelector('.answer').appendChild(p); //answer 자리에 붙인다.
+    };
+
+    let p = null;
+
+    recognition.start(); //음성 인식(녹음) 시작
+    recognition.onstart = function() {
+        makeNewTextContent(); // 음성 인식 시작시마다 새로운 문단을 추가한다.
+    };
+
+    /*//자동으로 종료될 때마다 다른 문장을 또 인식하기 위해 onend를 사용해 다시 시작할 수 있게 설정
+    recognition.onend = function() {
+        recognition.start();
+    };*/
+
+    recognition.onresult = function(e) {
+        let texts = Array.from(e.results)
+            .map(results => results[0].transcript).join(""); //우리가 원하는 인식된 문장은 results의 첫번째 Alternative값의 transcript에 담겨있다.
+
+        p.textContent = texts;
+    };
+
+    //"면접 끝내기" 버튼 누르면 인식 종료
+    /*endButton.onclick = function() {
+        recognition.abort();
+        console.log('Speech recognition aborted.');
+    };*/
+
+    endButton.addEventListener('click', function(){
+        recognition.abort();
+    });
 }
