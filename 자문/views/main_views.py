a5624d36b34e8e755cea9afad7155edb9f903469
myama-ame <55132630+myama-ame@users.noreply.github.com>
2021-11-23 00:04:34 +0900

    Update main_views.py
    /interview GET, POST 추가

--- 자문/views/main_views.py
+++ 자문/views/main_views.py
@@ -1,9 +1,11 @@
 # 화면 구성
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('main', __name__, url_prefix='/')
-q = ['자기소개를 해보세요. ', '자원봉사를 하면서 소모임장을 했다고 했는데, 모임장을 하게 된 동기는?', '팀 과제나 프로젝트를 하면서 어려움이 있었던 경우에 대해 말해보세요.']
+
+# getQuestion.py 위치에 따라 수정해주세요
+from web.views import getQuestion
 
 @bp.route('/')
 def hello() :
@@ -13,6 +15,13 @@ def hello() :
 def input() :
     return render_template('input.html')
 
-@bp.route('/interview')
+@bp.route('/interview', methods=('GET', 'POST'))
 def interview() :
-    return render_template('interview.html', question=q, question_n=len(q))
+    if request.method == 'POST' :
+        es = request.form.get('content')
+        q = getQuestion.get_question(es)
+        return render_template('interview.html', question=q, question_n=len(q))
+
+@bp.route('/introduce')
+def introduce() :
+    return render_template('introduce.html')
